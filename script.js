caa5d0bbfc988256326adb2267357cb17c24168c
AmeyPank <pankantiameya@gmail.com>
2023-03-13 16:31:21 +0530

    Bands In lexicographical order

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+const bandNames =  ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+function stripArticles(name) {
+  return name.replace(/^(a|an|the)\s+/i, '').trim();
+}
+
+const sortedBands = bandNames.sort((a, b) => stripArticles(a) > stripArticles(b) ? 1 : -1);
+
+const bandList = document.querySelector('#band');
+bandList.innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
 
